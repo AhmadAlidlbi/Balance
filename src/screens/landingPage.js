ee4886615775f9ff61434b49ca4d1eedabefb046
AhmadAlidlbi <200513470@ogr.altinbas.edu.tr>
2024-02-12 19:41:50 +0300

    landingpage

--- src/screens/landingPage.js
+++ src/screens/landingPage.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Image, StyleSheet, Button, Text } from "react-native";
 
 const LandingPage = () => {
   return (
     <View style={styles.container}>
-        <Image style={styles.logo} source={require('../assets/landingLogo.jpg')}/>
+      <Image
+        style={styles.logo}
+        source={require("../assets/landingLogo.jpg")}
+      />
+      <Text style={styles.balance}>Balance</Text>
+      <View style={styles.signupButton}>
+        <Button title="Sign Up" color="#FFFFFF" />
+      </View>
+      <View style={styles.loginButton}>
+        <Button title="Login" color="#FFFFFF" />
+      </View>
     </View>
   );
 };
@@ -12,12 +22,29 @@ const LandingPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  logo:{
-    width: 200,
-    height: 200
+  logo: {},
+  balance: {
+    fontSize: 30,
+    fontFamily: "poppins",
+    fontWeight: "400",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  signupButton: {
+    height: 40,
+    width: 254,
+    borderRadius: 20,
+    backgroundColor: "black",
+    marginBottom: 15,
+  },
+  loginButton: {
+    height: 40,
+    width: 254,
+    borderRadius: 20,
+    backgroundColor: "#3238FF",
   }
 });
 
