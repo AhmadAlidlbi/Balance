575b9c044b16f257ad8c4493d1553318e0bd3bc8
AhmadAlidlbi <200513470@ogr.altinbas.edu.tr>
2024-02-23 18:41:05 +0300

    app

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import { React } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import Main from "./src/screens/Main";
-import Login from "./src/screens/Login";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Main from "./src/screens/Main";
+import Login from "./src/screens/Login";
 import Auth from "./src/screens/Auth";
 import SecondAuth from "./src/screens/SecondAuth";
 import OnboardingTutorial from "./src/screens/OnboardingTutorial";
