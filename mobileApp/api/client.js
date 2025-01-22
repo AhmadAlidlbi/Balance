04f960a03ba095c4864a18ebda4dd4a37eec814c
Ahmad Alidlbi <ahmedidlbi@hotmail.com>
2025-01-22 23:14:29 +0300

    refactor: clean up unused axios client code

--- mobileApp/api/client.js
+++ mobileApp/api/client.js
@@ -1,27 +1,3 @@
-// import axios from "axios";
-// import { BASE_URL } from "../utils/config";
-// import { getToken } from "../utils/storage";
-
-// const ClientApi= axios.create({
-//   baseURL: BASE_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//     "authorization": `Bearer ${getToken()}`,
-//   },
-// });
-// const ClientFormDataApi= axios.create({
-//   baseURL: BASE_URL,
-//   headers: {
-//     "Content-Type": "multipart/form-data",
-//     "authorization": `Bearer ${getToken()}`,
-//   },
-// });
-
-// export { ClientFormDataApi };
-
-// export default ClientApi;
-
-
 import axios from "axios";
 import { BASE_URL } from "../utils/config";
 import { getToken } from "../utils/storage";
